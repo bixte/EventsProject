f263b1299ab0518da17bf0435e415a073e481ec8
artem <artm.isWorking@gmail.com>
2023-03-16 09:44:39 +0300

    Change of expression to function

--- EventsProject/Features/Events/Commands/AddEvent/AddEventCommandValidator.cs
+++ EventsProject/Features/Events/Commands/AddEvent/AddEventCommandValidator.cs
@@ -5,14 +5,25 @@ namespace EventsProject.Features.Events.Commands.AddEvent
 {
     public class AddEventCommandValidator :AbstractValidator<AddEventCommand>
     {
+        private readonly IDataBase dataBase;
+
         public AddEventCommandValidator(IDataBase dataBase)
         {
             RuleFor(e => e.Name).NotEmpty();
             RuleFor(e => e.Description).NotEmpty();
             RuleFor(e => e.DateStart).NotEmpty().LessThan(e => e.DateEnd);
             RuleFor(e => e.DateEnd).NotEmpty().GreaterThan(e => e.DateStart);
-            RuleFor(e => e.PictureId).NotNull().Must(id => dataBase.PictureRepository.Find(id) != null);
-            RuleFor(e => e.PlaceId).NotNull().Must(id => dataBase.PlaceRepository.Find(id) != null);
+            RuleFor(e => e.PictureId).NotNull().Must(BeInPictureRepository);
+            RuleFor(e => e.PlaceId).NotNull().Must(BeInPlaceRepository);
+            this.dataBase = dataBase;
+        }
+        private bool BeInPictureRepository(Guid id)
+        {
+           return dataBase.PictureRepository.Find(id) != null;
+        }
+        private bool BeInPlaceRepository(Guid id)
+        {
+           return dataBase.PlaceRepository.Find(id) != null;
         }
     }
 }
--- EventsProject/Features/Events/Commands/UpdateEvent/UpdateEventValidator.cs
+++ EventsProject/Features/Events/Commands/UpdateEvent/UpdateEventValidator.cs
@@ -5,6 +5,8 @@ namespace EventsProject.Features.Events.Commands.UpdateEvent
 {
     public class UpdateEventValidator:AbstractValidator<UpdateEventCommand>
     {
+        private readonly IDataBase dataBase;
+
         public UpdateEventValidator(IDataBase dataBase)
         {
             RuleFor(e => e.Id).NotNull();
@@ -12,8 +14,17 @@ namespace EventsProject.Features.Events.Commands.UpdateEvent
             RuleFor(e=>e.Description).NotEmpty();
             RuleFor(e=>e.DateStart).NotEmpty().LessThan(e=>e.DateEnd);
             RuleFor(e=>e.DateEnd).NotEmpty().GreaterThan(e=>e.DateStart);
-            RuleFor(e => e.PictureId).NotNull().Must(id => dataBase.PictureRepository.Find(id) != null);
-            RuleFor(e => e.PlaceId).NotNull().Must(id => dataBase.PlaceRepository.Find(id) != null);
+            RuleFor(e => e.PictureId).NotNull().Must(BeInPictureRepository);
+            RuleFor(e => e.PlaceId).NotNull().Must(BeInPlaceRepository);
+            this.dataBase = dataBase;
+        }
+        private bool BeInPictureRepository(Guid id)
+        {
+            return dataBase.PictureRepository.Find(id) != null;
+        }
+        private bool BeInPlaceRepository(Guid id)
+        {
+            return dataBase.PlaceRepository.Find(id) != null;
         }
     }
 }
